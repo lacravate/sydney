bec77e35d0a6bdd7865fbc22b44ba1a956b10216
lacravate <lacravate@lacravate.fr>
2023-03-06 10:31:58 +0000

    db/seeds.rb; using RecipePopulator

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,4 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-File.read('db/seeds.json').lines[0..0].each do |l|
-  h = JSON.parse l.chomp
-
-  Recipe.create h
-    .merge(searchable_ingredients: h['ingredients'].to_json)
-    .except('tags', 'image', 'nb_comments', 'author_tip', 'ingredients')
-end
+RecipePopulator.populate_with 'db/seeds.json'
